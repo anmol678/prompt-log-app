b3675a4bb671cce8de0e0092e3fd4330c1f783fe
Anmol Jain <anmolj678@gmail.com>
2023-06-12 05:10:30 -0700

    handle textarea sizing

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,18 +2,34 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
+    const innerRef = React.useRef<HTMLTextAreaElement | null>(null);
+
+    React.useEffect(() => {
+      if (props.value && innerRef.current) {
+        innerRef.current.style.height = 'auto';
+        innerRef.current.style.height = `${innerRef.current.scrollHeight}px`;
+      }
+    }, [props.value]);
+
+    React.useEffect(() => {
+      if (typeof ref === 'function') {
+        ref(innerRef.current);
+      } else if (ref) {
+        ref.current = innerRef.current;
+      }
+    }, [ref, innerRef]);
+
     return (
       <textarea
         className={cn(
-          "flex min-h-[calc(40vh)] w-full bg-transparent px-3 py-2 text-md placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "resize-none flex w-full h-auto bg-transparent px-3 py-2 text-md placeholder:text-muted-foreground focus-visible:outline-none read-only:max-h-[25vh] read-only:overflow-auto",
           className
         )}
-        ref={ref}
+        ref={innerRef}
         {...props}
       />
     )

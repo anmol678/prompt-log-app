3565cd0d5bd64d141fc526801908c1b9819d111a
Anmol Jain <anmolj678@gmail.com>
2023-07-11 14:50:09 +0530

    refresh onview for no-cache workaround

--- app/prompts/[id]/page.tsx
+++ app/prompts/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Grid } from "@tremor/react";
 import { PromptTemplate } from "@/types/prompt-template";
 import { promptTemplatesAPI as api } from "@/lib/client"
 import PromptTemplateDetail from "./_components/prompt-template-detail";
+import RefreshOnView from "@/components/refresh-onview";
 
 export default async function Page({ params }: { params: { id: string } }) {
     const promptTemplate: PromptTemplate = await api.getPromptTemplate(Number(params.id))
@@ -11,6 +12,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     return (
         <Grid numCols={1} className="gap-4 h-full flex flex-col">
             <PromptTemplateDetail template={promptTemplate} />
+            <RefreshOnView />
         </Grid>
     )
 }
--- app/prompts/page.tsx
+++ app/prompts/page.tsx
@@ -4,10 +4,11 @@ import PromptTemplateCard from "./_components/prompt-template-card";
 import { Heading2 } from "@/components/ui/heading";
 import { PromptTemplate } from "@/types/prompt-template";
 import { promptTemplatesAPI as api } from "@/lib/client"
+import RefreshOnView from "@/components/refresh-onview";
 
 export default async function Page() {
     const data: PromptTemplate[] = await api.getPromptTemplates()
-    
+
     return (
         <Card>
             <div className="flex justify-between items-center mb-6">
@@ -19,6 +20,7 @@ export default async function Page() {
                     <PromptTemplateCard key={template.id} template={template} />
                 ))}
             </div>
+            <RefreshOnView />
         </Card>
     )
 }
